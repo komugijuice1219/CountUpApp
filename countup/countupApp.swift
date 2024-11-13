bf3410b01050f41642a8db9ff9918aa92df1d7d9
tao <taozhedan1219@gmail.com>
2024-11-13 11:28:11 +0900

    Initial Commit

---
+++ countup/countupApp.swift
@@ -0,0 +1,17 @@
+//
+//  countupApp.swift
+//  countup
+//
+//  Created by たお1219 on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct countupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
